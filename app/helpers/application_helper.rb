2e0d08753c4fbfda693dcc947edade97278d0235
Ian Wambai <ianwambai@gmail.com>
2014-08-26 19:58:34 +0300

    Added a site layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def xoxo_logo
+		image_tag("xoxo_logo.png", :alt => "Xoxo logo", :id => "logo")
+	end
+	def playstore_logo
+		image_tag("play_store.png", :alt => "Play Store logo", :id => "logo")
+	end
+	def twitter_logo
+		image_tag("twitter.png", :alt => "Twitter logo")
+	end
+	def facebook_logo
+		image_tag("facebook.png", :alt => "Facebook logo")
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
- root :to => "pages#home"
+ root "pages#home"
+
+ get '/about' => 'pages#about'
+ get 'contact' => 'pages#contact'
+ get '/help' => 'pages#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
